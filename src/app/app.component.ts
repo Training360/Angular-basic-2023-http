c409dc2a8befc0e17beb1a3b0f81e582717b6441
cherryApp <j.cserko@gmail.com>
2023-08-14 16:18:50 +0200

    lesson-04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-directives';
+
+  isSearchBarVisible: boolean = false;
+
+  toggleSearchBar(): void {
+    this.isSearchBarVisible = !this.isSearchBarVisible;
+  }
 }
